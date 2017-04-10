a8f392eac9f5c42842df051ac89858c01b9e4bb6
G. B. Versiani <guibv@yahoo.com>
2017-04-09 23:37:23 -0300

    Bump to 0.1.1

--- README.md
+++ README.md
@@ -42,7 +42,7 @@ If [available in Hex](https://hex.pm/docs/publish), the package can be installed
 
     ```elixir
     def deps do
-      [{:sippet, "~> 0.1.0"}]
+      [{:sippet, "~> 0.1.1"}]
     end
     ```
 
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Sippet.Mixfile do
 
   def project do
     [app: :sippet,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
