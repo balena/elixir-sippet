5dc3eb1f7ce0ed23a96f979ee08a6522852980b0
G. B. Versiani <guibv@yahoo.com>
2017-04-12 19:05:40 -0300

    Improved the Core documentation.

--- lib/sippet/core.ex
+++ lib/sippet/core.ex
@@ -12,7 +12,14 @@ defmodule Sippet.Core do
   @doc """
   Receives a new incoming request from a remote host, or ACK.
 
-  The `server_transaction` 
+  The `server_transaction` indicates the name of the transaction created when
+  the request was received. If it is an ACK, then the `server_transaction` is
+  `nil`.
+
+  The function `receive_request/2` is called from the server transaction
+  process when the parameter `server_transaction` is not `nil`, and from the
+  transport process (possibly a `poolboy` worker process), when the
+  `server_transaction` is `nil`.
   """
   @callback receive_request(incoming_request :: Message.request,
                             server_transaction :: Transaction.Server.t | nil)
@@ -20,6 +27,14 @@ defmodule Sippet.Core do
 
   @doc """
   Receives a response for a sent request.
+
+  The `client_transaction` indicates the name of the transaction created when
+  the request was sent using `Sippet.Transaction.send_request/1`.
+
+  The function `receive_response/2` is called from the client transaction
+  process when the parameter `client_transaction` is not `nil`, and from the
+  transport process (possibly a `poolboy` worker process), when the
+  `client_transaction` is `nil`.
   """
   @callback receive_response(incoming_response :: Message.response,
                              client_transaction :: Transaction.Client.t | nil)
@@ -27,6 +42,9 @@ defmodule Sippet.Core do
 
   @doc """
   Sends receives an error from the transaction.
+
+  The function `receive_error/2` is called from the client or server
+  transaction process created when sending or receiving requests.
   """
   @callback receive_error(reason :: term,
                           client_or_server_transaction ::
