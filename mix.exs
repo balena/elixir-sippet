362c372962627ffce56d8aa59103ca14d1579f4c
G. B. Versiani <guibv@yahoo.com>
2017-04-19 09:04:00 -0300

    Bump to 0.1.8

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Sippet.Mixfile do
 
   def project do
     [app: :sippet,
-     version: "0.1.7",
+     version: "0.1.8",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
