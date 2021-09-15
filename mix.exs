0237410b6518143b7550ddb7c90599353d88457c
Guilherme Balena Versiani <guilherme@telnyx.com>
2021-09-14 22:25:09 -0300

    Made the version 0.6.x compatible with Erlang 23+.

--- mix.exs
+++ mix.exs
@@ -1,103 +1,75 @@
 defmodule Sippet.Mixfile do
   use Mix.Project
 
-  @version "0.6.3"
+  @version "0.6.4"
 
   def project do
-    [app: :sippet,
-     version: @version,
-     elixir: "~> 1.8",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     compilers: [:make] ++ Mix.compilers, # Add the make compiler
-     aliases: aliases(), # Configure aliases
-     deps: deps(),
-     package: package(),
-
-     name: "Sippet",
-     docs: [logo: "logo.png"],
-
-     source_url: "https://github.com/balena/elixir-sippet",
-     description: description(),
-
-     test_coverage: [tool: ExCoveralls],
-     preferred_cli_env: [
-       coveralls: :test,
-       "coveralls.detail": :test,
-       "coveralls.post": :test,
-       "coveralls.html": :test
-     ]]
-  end
-
-  defp aliases do
-    # Execute the usual mix clean and our Makefile clean task
-    [clean: ["clean", "clean.make"]]
+    [
+      app: :sippet,
+      version: @version,
+      elixir: "~> 1.8",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      compilers: [:elixir_make] ++ Mix.compilers(),
+      deps: deps(),
+      package: package(),
+      name: "Sippet",
+      docs: [logo: "logo.png"],
+      source_url: "https://github.com/balena/elixir-sippet",
+      description: description(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      make_clean: ["clean"],
+      make_cwd: "c_src"
+    ]
   end
 
   def application do
-    [applications: [:logger, :gen_state_machine, :socket, :poolboy],
-     mod: {Sippet.Application, []}]
+    [
+      applications: [:logger, :gen_state_machine, :socket, :poolboy],
+      mod: {Sippet.Application, []}
+    ]
   end
 
   defp deps do
-    [{:gen_state_machine, "~> 2.0"},
-     {:socket, "~> 0.3.13"},
-     {:poolboy, "~> 1.5"},
+    [
+      {:gen_state_machine, "~> 2.0"},
 
-     # Docs dependencies
-     {:ex_doc, "~> 0.19.3", only: :dev, runtime: false},
-     {:inch_ex, "~> 2.0", only: :docs},
+      # Build the NIF
+      {:elixir_make, "~> 0.4", runtime: false},
 
-     # Test dependencies
-     {:mock, "~> 0.3.3", only: :test},
-     {:excoveralls, "~> 0.10.6", only: :test},
-     {:credo, "~> 1.0", only: [:dev, :test]},
-     {:dialyxir, "~> 0.5", only: [:dev], runtime: false}]
+      {:socket, "~> 0.3.13"},
+      {:poolboy, "~> 1.5"},
+
+      # Docs dependencies
+      {:ex_doc, "~> 0.19.3", only: :dev, runtime: false},
+      {:inch_ex, "~> 2.0", only: :docs},
+
+      # Test dependencies
+      {:mock, "~> 0.3.3", only: :test},
+      {:excoveralls, "~> 0.10.6", only: :test},
+      {:credo, "~> 1.0", only: [:dev, :test]},
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
+    ]
   end
 
   defp description do
     """
-    An Elixir library designed to be used as SIP protocol middleware.
+    An Elixir Session Initiation Protocol (SIP) stack.
     """
   end
 
   defp package do
-    [maintainers: ["Guilherme Balena Versiani"],
-     licenses: ["BSD"],
-     links: %{"GitHub" => "https://github.com/balena/elixir-sippet"},
-     files: ~w"lib c_src/*.{h,cc} c_src/Makefile mix.exs README.md LICENSE"]
-  end
-end
-
-# Make tasks
-
-defmodule Mix.Tasks.Compile.Make do
-  # Compiles helper in c_src
-
-  def run(_) do
-    {result, error_code} = System.cmd("make", ["-C", "c_src"], stderr_to_stdout: true)
-
-    # XXX(balena): Because the compiler changes the priv directory, we need to
-    # notify Mix to rebuild the project structure under _build, copying the new
-    # priv files.
-    # https://github.com/riverrun/comeonin/pull/41/commits/5670e424f7d4feba0839211090f5dcf79b340577
-    if error_code == 0 do
-      Mix.Project.build_structure
-    end
-
-    Mix.shell.info result
-
-    :ok
-  end
-end
-
-defmodule Mix.Tasks.Clean.Make do
-  # Cleans helper in c_src
-
-  def run(_) do
-    {result, _error_code} = System.cmd("make", ["-C", "c_src", "clean"], stderr_to_stdout: true)
-    Mix.shell.info result
-
-    :ok
+    [
+      maintainers: ["Guilherme Balena Versiani"],
+      licenses: ["BSD"],
+      links: %{"GitHub" => "https://github.com/balena/elixir-sippet"},
+      files: ~w"lib c_src/*.{h,cc} c_src/Makefile mix.exs README.md LICENSE"
+    ]
   end
 end
